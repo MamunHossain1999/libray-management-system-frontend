4fdd7df918578ce83baca2169317fa06a65c3923
Your Name <youremail@example.com>
2025-07-05 12:56:36 +0600

    update file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import tailwindcss from '@tailwindcss/vite'
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react() ,tailwindcss(),],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
